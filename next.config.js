0bca991c1e5b8f6b85813c8cebfe2de94e7fbf4d
Arutik228 <arutiktopcik@gmail.com>
2026-04-03 17:56:21 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: '.',  // Экспорт в корень (папка portfolio)
   images: {
     unoptimized: true,
   },
