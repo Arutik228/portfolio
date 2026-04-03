bc10e331debde4a554b767b80cd86408a120b510
Arutik228 <arutiktopcik@gmail.com>
2026-04-03 15:17:11 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export',  // Включаем статический экспорт для GitHub Pages
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  // Если репозиторий называется portfolio, раскомментируйте:
-  // basePath: '/portfolio',
-  // assetPrefix: '/portfolio/',
 }
 
 module.exports = nextConfig
